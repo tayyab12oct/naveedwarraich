a82e8d4fcd459391dda3a697365a9d394c11556d
JafeelKhanzada <jafeelkhanzada@gmail.com>
2022-01-26 16:09:10 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Slider from "react-slick";
 
 function App() {
+  var settings = {
+    speed: 500,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#E8D814] min-h-[50vh] px-64 text-white p-10">
+      <h1 className="text-4xl font-bold pb-10 tracking-wide	">
+        <span className="text-black">SATISFIED</span> CUSTOMERS
+      </h1>
+      <Slider {...settings} className="h-full text-white  ">
+        {new Array(5).fill("1").map((v, k) => (
+          <div className="flex flex-col justify-center items-center  w-full px-24 h-[100%]">
+            <p className="font-bold text-5xl">""</p>
+            <p className="text-center text-md font-light py-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              vitae egestas mi, vel dapibus diam. Mauris malesuada, nisl non
+              rutrum commodo, sem magna laoreet tellus, eu euismod dolor enim et
+              mi. In at tempor purus.
+            </p>
+            <img
+              src="https://megaone.acrothemes.com/digital-agency/img/test1.jpg"
+              className="rounded-full pt-4"
+            />
+            <p className="text-black text-xl font-semibold pt-5 ">
+              Sara Williams
+            </p>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: true,
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
